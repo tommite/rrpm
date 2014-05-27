4318e3e941d239778f4536697e44b5b855ac664c
Tommi Tervonen <tervonen@ese.eur.nl>
2014-05-27 15:16:06 +0200

    Add Pd generation

---
+++ src/optimization.R
@@ -0,0 +1,28 @@
+library(ROI)
+
+## onLoad
+.solver <- NULL
+solvers <- ROI_installed_solvers()
+if (!is.na(solvers['symphony'])) {
+    .solver <<- 'symphony'
+} else if (!is.na(solvers['glpk'])) {
+    .solver <<- 'glpk'
+} else {
+    stop("No ROI Symphony or GLPK plugin installed")
+}
+
+optimize.pf <- function(values, costs, budget) {
+    values <- as.vector(values)
+    stopifnot(length(values) == length(costs))
+
+    m <- length(values)
+
+    obj.func <- L_objective(values)
+    constr <- L_constraint(costs, c('<='), budget)
+    ip <- OP(obj.func, constr, types=rep('I', m),
+             bounds=V_bound(1:m, 1:m, rep(0, m), rep(1, m)),
+             maximum=TRUE)
+    res <- ROI_solve(ip, .solver)
+    stopifnot(res$status$code == 0)
+    res$solution
+}
--- src/rpm.R
+++ src/rpm.R
@@ -1,4 +1,6 @@
 library(plyr)
+library(hitandrun)
+source('optimization.R')
 
 ###
 ## Computes RPM non-dominated portfolios
@@ -10,18 +12,34 @@ library(plyr)
 rpm.nondom <- function(projects, budget) {
     stopifnot(all(projects[,ncol(projects)] <= budget)) # PRECOND
     
+    m <- nrow(projects)
+    
     Pk <- matrix(0, ncol=m, nrow=2) # initial portfolios
     Pk[2,1] <- 1
     colnames(Pk) <- paste('x', 1:m, sep='')
 
+    Pd <- gen.Pd(projects, budget)
+
     ## round k = 1, ..., m
     for (k in 2:m) {
+        print(k)
         Pk.with.xk <- Pk
         Pk.with.xk[,k] = 1
         Pk.tilde <- rbind(Pk, filter.feasible(Pk.with.xk, projects, budget))
+        Pk <- Pk.tilde
     }
 }
 
+## Tries to generate size portfolios, some might be duplicates
+## so the return value might be with less rows
+gen.Pd <- function(projects, budget, size=100) {
+    proj.inds <- matrix(0, ncol=nrow(projects), nrow=size)
+    proj.data <- projects[,-ncol(projects)]
+    proj.costs <- projects[,ncol(projects)]
+    weights <- simplex.sample(ncol(projects)-1, 100)$samples
+
+    unique(aaply(weights, 1, function(w) { optimize.pf(w %*% t(proj.data), proj.costs, budget) }))
+}
 
 filter.feasible <- function(proj.inds, projects, budget) {
     total.use <- proj.inds %*% as.matrix(projects)
